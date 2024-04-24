e8680db8dc77bde90e9c867db60c1c9efd021eb0
animesh07kumar <159750224+animesh07kumar@users.noreply.github.com>
2024-04-24 13:50:07 +0530

    Create Snake water and gun game.js

---
+++ Snake water and gun game.js
@@ -0,0 +1,59 @@
+let max = 4
+let min = 1
+let random_number = Math.floor(Math.random() * (max - min) + min)
+random_number = Number.parseInt(random_number)
+console.log(random_number)
+let com = ""
+switch(random_number){
+    case 1:
+        com = "Snake"
+        break;
+    case 2:
+        com = "Water"
+        break;
+    case 3:
+        com = "Gun"
+        break;
+}
+console.log(com)
+let com_win_count = 0;
+let player_win_count = 0;
+let tie = 0;
+let ask = true;
+while(ask){
+    let inputed = prompt("Enter your choice among Snake, Water and Gun ")
+    if(com === Snake && inputed === Water){
+        com_win_count++;
+      }
+    else if(com === Snake && inputed === gun){
+        player_win_count++;
+      }
+    else if(com === Water && inputed === Snake){
+        player_win_count++;
+      }
+    else if(com === Water && inputed === Gun){
+        com_win_count++;
+      }
+    else if(com === Gun && inputed === Snake){
+        com_win_count++;
+      }
+    else if(com === Gun && inputed === Water){
+        player_win_count++;
+      }
+    else{
+        tie++;
+      }
+    ask = confirm(`Current Soure Computer Wins ${com_win_count}, Player Wins ${player_win_count} and tie Count ${tie}. do you Want to play again`)
+  }
+  if(com_win_count > player_win_count){
+    alert(`Computer is winner, Well Played try next time.`)
+    alert(`Final Soure Computer Wins ${com_win_count}, Player Wins ${player_win_count} and tie ${tie}.`)
+  }
+  else if(com_win_count < player_win_count){
+    alert(`Congratulations! You are winner, Well Played, Win again.`)
+    alert(`Final Soure Computer Wins ${com_win_count}, Player Wins ${player_win_count} and tie ${tie}.`)
+  }
+  else{
+    alert(`Nail biting match loved to play again with you.`)
+    alert(`Final Soure Computer Wins ${com_win_count}, Player Wins ${player_win_count} and tie ${tie}.`)
+  }
